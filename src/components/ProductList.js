269e9f7b14be1650a7e963b83b2d7f664d7aee1a
Efe <knowledgeagare157@gmail.com>
2019-10-12 17:23:55 +0100

    create a productlist component

---
+++ src/components/ProductList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ProductList extends Component {
+  state = {  }
+  render() { 
+    return ( <div>hello from product list</div> );
+  }
+}
+ 
+export default ProductList;
