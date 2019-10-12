3972d573848bde7b6db06865482863bcf356ff3f
Efe <knowledgeagare157@gmail.com>
2019-10-12 20:19:31 +0100

    finish the title component

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,10 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Product from './Product';
+import Title from './Title';
 class ProductList extends Component {
-  state = {  }
-  render() { 
-    return ( <Product/> );
+  state = {
+    products: []
+  }
+  render() {
+    return ( <Fragment>
+      <div className="py-5">
+        <div className="container">
+          <Title name="our" title="products"/>
+        </div>
+        <div className="row"></div>
+      </div>
+    </Fragment> );
   }
 }
- 
+
 export default ProductList;
