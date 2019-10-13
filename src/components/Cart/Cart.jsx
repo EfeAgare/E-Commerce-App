6f05a2fa8df2d27f3c61a2b89016bbd2dc21ebe1
Efe <knowledgeagare157@gmail.com>
2019-10-13 10:42:11 +0100

    create cart List

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Title from '../Title';
 import CartColumns from './CartColumns';
-// import EmptyCart from './EmptyCart'
+import EmptyCart from './EmptyCart'
 import { ProductConsumer } from '../../contextAPI/contextProvider'
 // import CartList from './CartList'
 // import CartTotals from './CartTotals'
@@ -9,8 +9,20 @@ export default class Cart extends Component {
   render() {
     return (
       <section>
-<Title name="your" title="cart"/>
-<CartColumns/>
+        <ProductConsumer>{(value) => {
+        const {cart} = value;
+        if (cart.length > 0) {
+          return (
+            <Fragment>
+            <Title name="your" title="cart"/>
+            <CartColumns/>
+            </Fragment>)
+        } else {
+          return <EmptyCart/>
+        }
+      }}
+        </ProductConsumer>
+
       </section>
 
 
